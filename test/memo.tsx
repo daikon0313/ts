10d204534aa050b0718c875064b4e5299744e548
daisuke harato <d.harato@datumstudio.jp>
2025-03-04 10:17:53 +0900

    add

--- test/memo.tsx
+++
@@ -1,7 +0,0 @@
-function totalArea(shape1: Shape, shape2: Shape): number {
-    return shape1.area() + shape2.area();
-  }
-  
-  const circle = new circle({ radius: 10 });
-  const rectangle = new rectangle({ width: 10, height: 20 });
-  totalArea(circle, rectangle); // CircleとRectangleをShapeとして扱える
